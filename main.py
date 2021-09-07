fa9326f5f199462187629cdd0d4ace34eb1e4bba
justhleb <90176227+justhleb@users.noreply.github.com>
2021-09-07 22:08:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+# password="R2xlYgo="
+def encript(mpass):
+    #print("hi")
+    base64Table = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+                   'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+                   'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                   't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+                   '8', '9', '+', '/']
+    # print(mpass)
+    binword = []
+    for i in range(len(mpass)):
+        binword.append('0' + format(ord(mpass[i]), 'b'))
+    # print(binword)
+    binword = ''.join(binword)
+    # print(binword)
+    if len(binword) % 6 != 0:
+        while len(binword) % 6 != 0:
+            binword = binword + "0"
+    # print(binword)
+    sixword = []
+    # print(len(binword) / 6)
+    for i in range(int(len(binword) / 6)):
+        sixword.append('')
+        for j in range(6):
+            sixword[i] = sixword[i] + binword[i * 6 + j]
+    # print(sixword)
+    for i in range(len(sixword)):
+        sixword[i] = base64Table[int(sixword[i], base=2)]
+
+    sixword = ''.join(sixword)
+    if len(mpass) % 3 == 1:
+        sixword += '='
+        sixword += '='
+    if len(mpass) % 3 == 2:
+        sixword += '='
+    # print(sixword)
+    return sixword
+
+
+password = 'X1Xw=='
+
+encript(password)
+print(len(password))
+arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+       'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+       'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+       't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+       '8', '9', '_']
+for i in range(63):
+    if password == encript(arr[i]):
+        print("access granted your password: ", arr[i])
+        quit(0)
+print("not 1")
+two = [' ', ' ']
+for i in range(63):
+    for j in range(63):
+        two[0] = arr[i]
+        two[1] = arr[j]
+        #print(password, ' ', two, "\n")
+        if password == encript(two):
+            print("access granted your password: ", ''.join(two))
+            quit(0)
+print("not 2")
+three = [' ', ' ', ' ']
+for i in range(63):
+    for j in range(63):
+        for k in range(63):
+            three[0] = arr[i]
+            three[1] = arr[j]
+            three[2] = arr[k]
+            # print(password,' ',three,"\n")
+            if password == encript(three):
+                print("access granted your password: ", ''.join(three))
+                quit(0)
+print('not 3')
+four=[' ',' ',' ',' ']
+for i in range(63):
+    for j in range(63):
+        for k in range(63):
+            for t in range(63):
+                four[0] = arr[i]
+                four[1] = arr[j]
+                four[2] = arr[k]
+                four[3] = arr[t]
+                # print(password,' ',three,"\n")
+                if password == encript(four):
+                    print("access granted your password: ", ''.join(four))
+                    quit(0)
